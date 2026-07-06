b0c9eb77cd2f52e5b888a32f75cd51d624f16a26
mashalusman <61388943+mashalusman@users.noreply.github.com>
2026-07-06 14:43:26 +0500

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/your-repository-name/', // 👈 REPLACE THIS WITH YOUR EXACT REPOSITORY NAME
 })
