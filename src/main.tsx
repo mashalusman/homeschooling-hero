da73bf92f645770384cd92fd8bbf5bb3b1dee7cb
mashalusman <61388943+mashalusman@users.noreply.github.com>
2026-07-06 14:39:29 +0500

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AcademicHubHero from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AcademicHubHero />
+  </React.StrictMode>,
+)
